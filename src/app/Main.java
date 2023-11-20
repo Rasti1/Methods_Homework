05de31c5019336855b809303743aeca09f935fc1
Rasti1 <rostrostov1999@gmail.com>
2023-11-20 15:21:42 +0200

    Methods fixes commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -13,32 +13,36 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input sum of your purchase: ");
         purchaseAmount = sc.nextDouble();
-        calculationDiscount();
-        calculationFinalPrice();
-        conclusion();
+        discount = calculationDiscount();
+        totalPrice = calculationFinalPrice();
+        getOutput();
     }
 
-    private static void calculationDiscount() {
+    private static double calculationDiscount() {
         if (purchaseAmount <= 5000) {
             discount = purchaseAmount * 5 / 100;
             System.out.println("\nYou have got 5% discount! It is equals: "
                                   + discount + CURRENCY);
+            return discount;
         } else if (5000 < purchaseAmount && purchaseAmount <= 10000){
             discount =  purchaseAmount * 10 / 100;
             System.out.println("\nYou have got 10% discount! It is equals: "
                                   + discount + CURRENCY);
+            return discount;
         } else {
             discount = purchaseAmount * 15 / 100;
             System.out.println("\nYou have got 15% discount! It is equals: "
                                   + discount + CURRENCY);
+            return discount;
         }
     }
 
-    private static void calculationFinalPrice(){
+    private static double calculationFinalPrice(){
         totalPrice = purchaseAmount - discount;
+        return totalPrice;
     }
 
-    private static void conclusion(){
+    private static void getOutput(){
         System.out.println("\nIn conclusion:\n");
         System.out.printf("Total amount of your purchase: %.2fUSD%n", purchaseAmount);
         System.out.printf("Your discount: %.2fUSD%n", discount);
