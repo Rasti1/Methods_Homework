f2550087177bb2b6b84fddb5886b496a9aeb2338
Rasti1 <rostrostov1999@gmail.com>
2023-11-20 14:42:32 +0200

    Fixes commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    static double tip;
+    static double purchaseAmount;
     static double discount;
     static double totalPrice;
     private static final String CURRENCY = "USD";
@@ -12,35 +12,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input sum of your purchase: ");
-        tip = sc.nextDouble();
+        purchaseAmount = sc.nextDouble();
         calculationDiscount();
         calculationFinalPrice();
         conclusion();
     }
 
     private static void calculationDiscount() {
-        if (tip <= 5000) {
-            discount = tip * 5 / 100;
+        if (purchaseAmount <= 5000) {
+            discount = purchaseAmount * 5 / 100;
             System.out.println("\nYou have got 5% discount! It is equals: "
                                   + discount + CURRENCY);
-        } else if (5000 < tip && tip <= 10000){
-            discount =  tip * 10 / 100;
+        } else if (5000 < purchaseAmount && purchaseAmount <= 10000){
+            discount =  purchaseAmount * 10 / 100;
             System.out.println("\nYou have got 10% discount! It is equals: "
                                   + discount + CURRENCY);
         } else {
-            discount = tip * 15 / 100;
+            discount = purchaseAmount * 15 / 100;
             System.out.println("\nYou have got 15% discount! It is equals: "
                                   + discount + CURRENCY);
         }
     }
 
     private static void calculationFinalPrice(){
-        totalPrice = tip - discount;
+        totalPrice = purchaseAmount - discount;
     }
 
     private static void conclusion(){
         System.out.println("\nIn conclusion:\n");
-        System.out.printf("Total amount of your purchase: %.2fUSD%n", tip);
+        System.out.printf("Total amount of your purchase: %.2fUSD%n", purchaseAmount);
         System.out.printf("Your discount: %.2fUSD%n", discount);
         System.out.printf("To pay: %.2fUSD%n", totalPrice);
     }
